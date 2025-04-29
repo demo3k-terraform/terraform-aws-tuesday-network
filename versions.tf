2ec532c0a50e5e5f0a2814ce07fd6f7da16bd3ea
Demo3kTerraform <demo-3k@terraform.training>
2025-04-29 14:39:59 +0000

    updated versions.tf

--- versions.tf
+++ versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 5.96"
     }
   }
-  required_version = ">= 1.3.0"
+  required_version = "~> 1.11"
 }
